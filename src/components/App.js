e22e23f9375c994f70745aeedf5789b3dab3e3f6
Albert Sargsyan <sargsyan.abet@gmail.com>
2021-04-17 22:09:16 +0400

    Project Start

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography } from '@material-ui/core/';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h1">
+        IDTravel
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
