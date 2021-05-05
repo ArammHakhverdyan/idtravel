9b64431e8bf29b02c3d4cfca09993a6f6d3ad344
Albert Sargsyan <sargsyan.abet@gmail.com>
2021-05-06 02:11:37 +0400

    Tour Book (partly): Pinachityun

--- src/components/tour/TourLists.js
+++ src/components/tour/TourLists.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography, Grid } from '@material-ui/core';
+import { Card, CardActionArea, CardContent, CardMedia, Typography, Grid } from '@material-ui/core';
 import { useRouteMatch } from 'react-router';
 import { Link } from 'react-router-dom';
 import { db } from '../../config/config';
-
 
 const TourLists = () => {
     let { url } = useRouteMatch();
@@ -13,43 +12,21 @@ const TourLists = () => {
     const fetchTours = async () => {
         const response = db.collection('tours');
         const data = await response.get();
-        const asd = []
+        const dataArr = [];
         data.docs.forEach(item => {
-            asd.push(item.data())
+            dataArr.push(item.data())
         })
-        setTours(asd)
+        setTours(dataArr)
     }
     useEffect(() => {
         fetchTours();
     }, [])
 
-    console.log(tours)
-
-    // const tours = [
-    //     {
-    //         id: 1,
-    //         imgUrl: "http://fairytour.am/upload/nyuter/40b265.jpg",
-    //         title: "Georgia, Armenia and Iran",
-    //         date: "15.03.2021-22.03.2021",
-    //         description: "This tour shows you all the treasures of these 3 amazing countries – from the legendary UNESCO World Heritage Sites, spectacular natural beauty, mountain monasteries, ancient trade route caravanseries and unique cuisines, to the hidden gems of local lives. They are the iconic witnesses of time long past, this is where Europe and the Middle East meet with Central Asia. Follow in pilgrims’ footsteps and explore the history, spirit and the diversity of this incredible region. It’s time to discover amazing countries of the Silk Road!",
-    //         price: "300$",
-    //         day: 1,
-    //     },
-    //     {
-    //         id: 2,
-    //         imgUrl: "https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png",
-    //         title: "asd",
-    //         date: "dsa",
-    //         description: "gfhgfhgfhf!",
-    //         price: "10$",
-    //         day: 2,
-    //     },
-    // ]
 
     return (
         <Grid container spacing={3}>
             {tours && tours.map((item) =>
-                <Grid item xs={4} key={item.date}>
+                <Grid item xs={4} key={item.id}>
                     <Card>
                         <CardActionArea>
                             <Link to={`${url}/${item.id}`} style={{ textDecoration: "none" }}>
@@ -60,17 +37,12 @@ const TourLists = () => {
                                     <Typography gutterBottom variant="h5" component="h2">
                                         {item.title}
                                     </Typography>
-                                    <Typography variant="body1" color="textSecondary">
+                                    <Typography align="center" variant="body1" color="textSecondary">
                                         {item.date}
                                     </Typography>
                                 </CardContent>
                             </Link>
                         </CardActionArea>
-                        <CardActions style={{ justifyContent: "center" }}>
-                            <Button color="primary">
-                                Book Now
-                            </Button>
-                        </CardActions>
                     </Card>
                 </Grid>
             )}
--- src/components/tour/TourView.js
+++ src/components/tour/TourView.js
@@ -1,37 +1,31 @@
-import React from 'react';
-import { Grid, Typography, Button, Container, Box } from '@material-ui/core/';
+import React, { useEffect, useState } from 'react';
+import { Grid, Typography, Container, Box } from '@material-ui/core/';
 import TourTimeLine from '../tour/TourTimeLine';
 import { useParams } from 'react-router';
+import { db } from '../../config/config';
+import TourBook from './TourBook';
 
 const TourView = () => {
-
-    const data = [
-        {
-            id: "1",
-            imgUrl: "http://fairytour.am/upload/nyuter/40b265.jpg",
-            title: "Georgia, Armenia and Iran",
-            date: "15.03.2021-22.03.2021",
-            description: "This tour shows you all the treasures of these 3 amazing countries – from the legendary UNESCO World Heritage Sites, spectacular natural beauty, mountain monasteries, ancient trade route caravanseries and unique cuisines, to the hidden gems of local lives. They are the iconic witnesses of time long past, this is where Europe and the Middle East meet with Central Asia. Follow in pilgrims’ footsteps and explore the history, spirit and the diversity of this incredible region. It’s time to discover amazing countries of the Silk Road!",
-            price: "300$",
-            day: 1,
-        },
-        {
-            id: "2",
-            imgUrl: "https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png",
-            title: "asd",
-            date: "dsa",
-            description: "gfhgfhgfhf!",
-            price: "10$",
-            day: 2,
-        },
-    ]
-
     let { id } = useParams();
+    const [tours, setTours] = useState([])
+
+    const fetchTours = async () => {
+        const response = db.collection('tours');
+        const data = await response.get();
+        const dataArr = [];
+        data.docs.forEach(item => {
+            dataArr.push(item.data())
+        })
+        setTours(dataArr)
+    }
+    useEffect(() => {
+        fetchTours();
+    }, [])
 
     return (
         <Container>
             <Box py={4}>
-                {data.filter(tour => tour.id === id)
+                {tours.filter(tour => tour.id === id)
                     .map((tour) =>
                         <Grid key={tour.id} container spacing={3}>
                             <Grid item xs={5}>
@@ -47,15 +41,13 @@ const TourView = () => {
                                 <Typography gutterBottom variant="h6">
                                     Price {tour.price}
                                 </Typography>
-                                <Button variant="outlined" color="primary">
-                                    Book Now
-                                </Button>
+                                <TourBook />
                             </Grid>
                         </Grid>
                     )}
                 <Grid container spacing={3}>
                     <Grid item xs={12}>
-                        <TourTimeLine data={data} />
+                        <TourTimeLine data={tours} />
                     </Grid>
                 </Grid>
             </Box>
