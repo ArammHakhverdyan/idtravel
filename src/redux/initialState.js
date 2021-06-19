1a1fe3ed265a9f707f7bba49611c95cd72e3c33e
Arammyan <arammyan@gmail.com>
2021-06-19 15:17:08 +0400

    Updates

--- src/redux/initialState.js
+++
@@ -1,7 +0,0 @@
-export const initialState = {
-    auth: {
-        authError: null,
-        loggedInUser: null
-    },
-    users: {}
-}
--- src/redux/selectors.js
+++
@@ -1,6 +0,0 @@
-export const selectLoggedInUser = (state) => state.auth.loggedInUser;
-
-export const selectLoggedInUserId = (state) => state.auth.loggedInUser?.uid;
-
-export const selectLoggedInUserInfo = (state) => state.auth.loggedInUser?.info;
-
